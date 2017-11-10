1275c59671d1fc9469add42837355bf15d0a1e0f
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-11-11 01:08:52 +0200

    Add homework 1 dealing with Value Objects

---
+++ Project_Value_Object/Project_Value_Object/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Value_Object
+{
+    class Conference
+    {
+        private readonly string _ConferenceName;
+        public string ConferenceName
+        {
+            get { return _ConferenceName; }
+        }
+
+        private readonly DateTime _ConferenceDate;
+        public DateTime ConferenceDate
+        {
+            get { return _ConferenceDate;  }
+        }
+
+        public Conference(string ConferenceName, DateTime ConferenceDate)
+        {
+            this._ConferenceName = ConferenceName;
+            this._ConferenceDate = ConferenceDate;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if((obj==null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Conference conf = (Conference)obj;
+                return (ConferenceName.Equals(conf.ConferenceName)) && (ConferenceDate.Equals(conf.ConferenceDate));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return ConferenceName.GetHashCode() ^ ConferenceDate.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Conference({0}, {1})", ConferenceName, ConferenceDate);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ai = "a";
+            string bi = "a";
+            Console.WriteLine(ai.Equals(bi));
+        }
+    }
+}
